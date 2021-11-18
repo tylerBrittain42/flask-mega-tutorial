e9840f407dc100700ff962d6a5dbd220d609da4c
tbrittain2 <tbrittain2@ucmerced.edu>
2021-11-18 11:19:44 -0800

    lesson 3 completed

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,4 +19,11 @@ def index():
 
     mock_user = {'username':'Tyler'}
     return render_template('index.html',title='home',user=mock_user, posts=mock_posts)
-    # return 'Hello'
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember me={form.remember_me.data}')
+        return redirect(url_for('index'))    
+    return render_template('login.html',title='Sign in', form=form)

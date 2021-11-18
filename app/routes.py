1b08d8b49569e6a472177901f3d0f72b26d26fb9
tylerBrittain42 <tbrittain100@gmail.com>
2021-11-18 00:07:30 -0800

    incomplete lesson 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello'
+    mock_user = {'username':'Tyler'}
+    return render_template('index.html',title='home',user=mock_user)
+    # return 'Hello'

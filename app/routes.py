02564252f88dc3ae4b79b6046d614728fc39e483
tbrittain2 <tbrittain2@ucmerced.edu>
2021-11-18 10:28:54 -0800

    lesson 2 completed

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+
+    mock_posts = [
+        {
+            'author': {'username':'Billy'},
+            'body': 'I like trains'
+        },
+        {
+            'author': {'username':'Steven'},
+            'body': 'Boats are the superior transportation engine'
+        }
+    ]
+
     mock_user = {'username':'Tyler'}
-    return render_template('index.html',title='home',user=mock_user)
+    return render_template('index.html',title='home',user=mock_user, posts=mock_posts)
     # return 'Hello'

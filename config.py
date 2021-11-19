07e16998685baa57efe7fbe79ef17fda91114328
tbrittain2 <tbrittain2@ucmerced.edu>
2021-11-18 18:22:43 -0800

    migrating to desktop

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'This-is-not-a-safe-string'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///mega.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

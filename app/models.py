95eccdd321d242ef7bebea52906c1800d76f6e6f
tylerBrittain42 <tbrittain100@gmail.com>
2021-11-18 21:58:48 -0800

    eod

--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
 from sqlalchemy.orm import defaultload
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    # db.relationship typically defined on the "one" side
+    # allows us to access the "many" from the "one"
+    # backref creates the name of the field that links the "many" to the "one"
     posts = db.relationship('Post',backref='author', lazy='dynamic')
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -16,7 +28,13 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    # We store the foreign key on the "many" side of the relationship
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login.utils import login_required, login_user
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-
     mock_posts = [
         {
             'author': {'username':'Billy'},
@@ -17,13 +21,49 @@ def index():
         }
     ]
 
-    mock_user = {'username':'Tyler'}
-    return render_template('index.html',title='home',user=mock_user, posts=mock_posts)
+    return render_template('index.html',title='home', posts=mock_posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember me={form.remember_me.data}')
-        return redirect(url_for('index'))    
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)    
     return render_template('login.html',title='Sign in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+# @app.route('/user/<username>')
+# @login_required
+# def user(username):
+#     user = User.query.filter_by(username=username).first_or_404
+#     posts = [
+#         {
+#             'author': user,
+#             'body': 'I like trains'
+#         },
+#         {
+#             'author': user,
+#             'body': 'Boats are the superior transportation engine'
+#         }
+#     ]

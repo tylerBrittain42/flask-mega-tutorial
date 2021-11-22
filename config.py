9e6b92f48ac9112db49cb72deccc9ed34f801d75
tylerBrittain42 <tbrittain100@gmail.com>
2021-11-21 20:37:47 -0800

    Completed lesson 7

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'This-is-not-a-safe-string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///mega.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # configuring flask to send email right after error
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']

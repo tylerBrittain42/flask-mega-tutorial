43fb3767cb2f0e8aa59abf696a5d055a9260c638
tylerBrittain42 <tbrittain100@gmail.com>
2021-12-29 00:01:34 -0800

    ch11 completed

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 
